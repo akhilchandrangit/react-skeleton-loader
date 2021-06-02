52c5e5aba69af4d73a2c9359840c925424fa6c1d
akhil-chandran <akhil.chandran@neoito.com>
2021-06-02 10:12:28 +0530

    folder structure and basic functionalities

---
+++ src/SkeletonLoader.stories.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withKnobs, text, boolean, color } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
+
+import README from './README.md';
+import SkeletonLoader from './SkeletonLoader';
+
+const stories = storiesOf('SkeletonLoader', module);
+
+stories.addDecorator(withKnobs);
+stories.addParameters({
+  readme: {
+    content: README
+  }
+});
+
+stories.add('default', () => {
+  return (
+    <SkeletonLoader
+      width={100}
+      height={100}
+    />
+  );
+});
+
+stories.add('with count', () => {
+  return (
+    <SkeletonLoader
+      width={150}
+      height={30}
+      count={4}
+    />
+  );
+});
+
+stories.add('with custom style', () => {
+  return (
+    <SkeletonLoader
+      width={150}
+      height={30}
+      count={4}
+      style={{
+        marginRight: '20px',
+        borderRadius: '6px',
+      }}
+    />
+  );
+});
+
+stories.add('circle', () => {
+  return (
+    <SkeletonLoader
+      width={100}
+      height={100}
+      shape="circle"
+    />
+  );
+});
---
+++ src/SkeletonLoader.tsx
@@ -0,0 +1,88 @@
+import React, { useState, ReactNode, useEffect } from 'react';
+import  styled, { keyframes }  from  'styled-components';
+
+export type Shape = 'rectangle' | 'circle';
+
+export interface Props {
+  width: number | string;
+  height: number | string;
+  count?: number;
+  className?: string;
+  wrapperClass?: string;
+  color?: string;
+  style?: React.CSSProperties;
+  shape?: Shape;
+}
+
+const Sprinkle = keyframes`
+  0% {
+    background-position: -100px;
+  }
+  40%, 100% {
+    background-position: 140px;
+  }
+`;
+
+const Node = styled.div`
+  float: left;
+  width: 140px;
+  height: 16px;
+  margin-top: 12px;
+  border-radius: 4px;
+  background-image: linear-gradient(90deg, #ddd 0px, #e8e8e8 40px, #ddd 80px);
+  background-size: 600px;
+  animation: ${Sprinkle} 1.6s infinite linear;
+  margin-right: 4px;
+`;
+
+const SkeletonLoader: React.FC<Props> = ({
+  count = 1,
+  width,
+  height,
+  className = '',
+  wrapperClass = '',
+  color,
+  style,
+  shape = 'rectangle',
+}) => {
+  const [elements, setElements] = useState<ReactNode[] | null>(null);
+
+  useEffect(() => {
+    setElements(() => {
+      let nodes: ReactNode[]  = [];
+      const nodeStyle: React.CSSProperties = {
+        ...style,
+        width,
+        height,
+      }
+
+      if (shape === 'circle') {
+        nodeStyle.borderRadius = '50%';
+      }
+
+      for (let i = 0 ; i < count ; i++) {
+        nodes.push(
+          <Node
+            className={`react-skeleton-loader-item ${className}`}
+            style={nodeStyle}
+          />
+        );
+      }
+
+      return nodes;
+    });
+  }, []);
+
+  return (
+    <div className={`react-skeleton-loader ${wrapperClass}`}>
+      {elements?.map((element: ReactNode) => (
+        <>
+          {element}
+        </>
+      ))}
+    </div>
+  );
+};
+
+export default SkeletonLoader;
+
